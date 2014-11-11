eecc0d6b18de02e85fed5d081b49df0248b28090
Oi <for_apps@hush.com>
2014-11-10 23:51:56 -0500

    Configuring necessary gems for mailing-2

---
+++ app/mailers/purchase_mailer.rb
@@ -0,0 +1,15 @@
+class PurchaseMailer < ActionMailer::Base
+	layout 'purchase_mailer'
+	default from "One Month Stripe Payments <payments@onemonth.com>"
+
+	def purchase_receipt purchase
+		@purchase = purchasemail to: purchase.email, subject "Thanks for your purchase!"
+	end
+end
+=begin
+layout 'purchase_mailer'
+ default from: "One Month Commerce <commerce@onemonth.com>"
+ def purchase_receipt purchase
+ @purchase = purchase
+ mail to: purchase.email, subject: "Thanks for your purchase!"
+  end=end
--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,5 +1,14 @@
 class Purchase < ActiveRecord::Base
+
+	after_create :email_purchaser
 	def to_param
 		uuid
 	end
+
+	def email_purchaser
+		PurchaseMailer.purchase_peceipt(self).deliver
+
+	end
+
+
 end
